43f932c2ad0b483c33b0545931d80e3a75f6f950
ProgrammingHero1 <mahirruphy@gmail.com>
2025-01-11 21:11:03 +0600

    first commit

---
+++ src/router/router.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <h2>Route not found</h2>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+export default router;
